8a7a5a0dca6e7a1820680c20ecc20f726d7196d2
p-mar <78031154+p-mar@users.noreply.github.com>
2023-01-30 16:49:07 +0100

    serializer upd

--- ferapp/serializer.py
+++ ferapp/serializer.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
-from .models import Image
+from .models import Image, Video, File
 
 class ImageSerializer(serializers.ModelSerializer):
     image=serializers.ImageField()
     class Meta:
         model=Image
+        fields='__all__'
+
+class VideoSerializer(serializers.ModelSerializer):
+    video=serializers.VideoField()
+    class Meta:
+        model=Video
+        fields='__all__'
+
+class FileSerializer(serializers.ModelSerializer):
+    file=serializers.FileField()
+    class Meta:
+        model=File
         fields='__all__'
