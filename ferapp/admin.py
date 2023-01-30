7b5a6222db767df6f656f91138d6541eb16732e8
p-mar <78031154+p-mar@users.noreply.github.com>
2023-01-30 16:50:21 +0100

    Update admin.py

--- ferapp/admin.py
+++ ferapp/admin.py
@@ -1,17 +1,9 @@
 from django.contrib import admin
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 from .models import Video, Image
 
 # Register your models here.
 
 admin.site.register(Video)
 admin.site.register(Image)
-=======
 
-# Register your models here.
->>>>>>> 9bb8654c0e1fa2d1238491e18278cf49604aa637
-=======
-
-# Register your models here.
->>>>>>> ee9aea8311262ab1ae6a28270a51f7fa736894f5
