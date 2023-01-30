4e9ee50e79250c8a2869b3ca5c4c22e79852ded9
p-mar <78031154+p-mar@users.noreply.github.com>
2023-01-30 16:48:40 +0100

    Update urls.py

--- ferapp/urls.py
+++ ferapp/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     path('help/', views.about, name='help'),
     path('contact/', views.contact, name='contact'),
     path('history/', views.history, name='history'),
@@ -14,15 +13,7 @@ urlpatterns = [
     path('upload_video/download', views.download_file_video),
     path('get/',views.getImage),
     path('post/',views.postImage),
-]
-=======
-=======
->>>>>>> ee9aea8311262ab1ae6a28270a51f7fa736894f5
-    path('upload_video/', views.upload_video, name='upload_video'),
-    path('upload_image/', views.upload_image, name='upload_image'),
+
 ]
 
-<<<<<<< HEAD
->>>>>>> 9bb8654c0e1fa2d1238491e18278cf49604aa637
-=======
->>>>>>> ee9aea8311262ab1ae6a28270a51f7fa736894f5
+
