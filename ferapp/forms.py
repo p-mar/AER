a61aae2ec89f6fbe3121c2e02df139e4e04de498
p-mar <78031154+p-mar@users.noreply.github.com>
2023-01-30 16:49:48 +0100

    Update forms.py

--- ferapp/forms.py
+++ ferapp/forms.py
@@ -1,13 +1,7 @@
 from django import forms
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 from .models import Image, Video
-=======
-from .models import Image
->>>>>>> 9bb8654c0e1fa2d1238491e18278cf49604aa637
-=======
-from .models import Image
->>>>>>> ee9aea8311262ab1ae6a28270a51f7fa736894f5
+
 
 class UploadFileForm(forms.Form):
     file = forms.FileField()
@@ -15,17 +9,13 @@ class UploadFileForm(forms.Form):
 class ImageForm(forms.ModelForm):
     class Meta:
         model = Image
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         fields = ('title','image',)
 
 class VideoForm(forms.ModelForm):
     class Meta:
         model= Video
         fields= ['title','video',]
-=======
-        fields = ('image',)
->>>>>>> 9bb8654c0e1fa2d1238491e18278cf49604aa637
-=======
-        fields = ('image',)
->>>>>>> ee9aea8311262ab1ae6a28270a51f7fa736894f5
+
+
+
